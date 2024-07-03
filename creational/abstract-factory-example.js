a8e27615f8df08e4a2fd676091a0eaeeba0a3a78
Andrew Akimov <akimov.it.dev@gmail.com>
2024-07-03 20:49:38 +0300

    Made Abstract Factory example more dynamic

--- creational/abstract-factory-example.js
+++ creational/abstract-factory-example.js
@@ -4,33 +4,43 @@ class UIFactory {
   createButton() {
     throw new Error(ABSTRACT_METHOD_ERR)
   }
-  
+
   createInput() {
     throw new Error(ABSTRACT_METHOD_ERR)
   }
 }
 
 class MaterialUIFactory extends UIFactory {
+  static name = 'Material Design'
+
   createButton() {
-    return new MaterialButton()
+    return new MaterialButton(this.constructor.name)
   }
 
   createInput() {
-    return new MaterialInput()
+    return new MaterialInput(this.constructor.name)
   }
 }
 
 class IOSUIFactory extends UIFactory {
+  static name = 'iOS'
+
   createButton() {
-    return new IOSButton()
+    return new IOSButton(this.constructor.name)
   }
 
   createInput() {
-    return new IOSInput()
+    return new IOSInput(this.constructor.name)
   }
 }
 
 class Button {
+  static name = 'Button'
+
+  constructor(type) {
+    this.type = type
+  }
+
   render() {
     throw new Error(ABSTRACT_METHOD_ERR)
   }
@@ -38,17 +48,23 @@ class Button {
 
 class MaterialButton extends Button {
   render() {
-    return 'Rendering: Button: Material Design'
+    return `Rendering: ${super.constructor.name} (${this.type})`
   }
 }
 
 class IOSButton extends Button {
   render() {
-    return 'Rendering: Button: iOS'
+    return `Rendering: ${super.constructor.name} (${this.type})`
   }
 }
 
 class Input {
+  static name = 'Input'
+
+  constructor(type) {
+    this.type = type
+  }
+
   render() {
     throw new Error(ABSTRACT_METHOD_ERR)
   }
@@ -56,13 +72,13 @@ class Input {
 
 class MaterialInput extends Input {
   render() {
-    return 'Rendering: Input: Material Design'
+    return `Rendering: ${super.constructor.name} (${this.type})`
   }
 }
 
 class IOSInput extends Input {
   render() {
-    return 'Rendering: Input: iOS'
+    return `Rendering: ${super.constructor.name} (${this.type})`
   }
 }
 
@@ -74,9 +90,9 @@ function createUI(factory) {
 }
 
 const materialUI = createUI(new MaterialUIFactory())
-console.log(materialUI.button.render()) // Rendering: Button: Material Design
-console.log(materialUI.input.render()) // Rendering: Input: Material Design
+console.log(materialUI.button.render()) // Rendering: Button (Material Design)
+console.log(materialUI.input.render()) // Rendering: Input (Material Design)
 
 const iosUI = createUI(new IOSUIFactory())
-console.log(iosUI.button.render()) // Rendering: Button: iOS
-console.log(iosUI.input.render()) // Rendering: Input: iOS
+console.log(iosUI.button.render()) // Rendering: Button (iOS)
+console.log(iosUI.input.render()) // Rendering: Input (iOS)
